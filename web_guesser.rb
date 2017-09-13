e9a6a976c08b66b5e1a18a06ac4f6fd9792f02ce
Vaughn Antonyan <vaughnanton@gmail.com>
2017-09-13 15:49:00 -0700

    I2: game functionality complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-num = rand(100).to_s
+$secret_number = rand(100)
 
 get '/' do
-  erb :index, :locals => {:num => num}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:number => $secret_number, :message => message}
+end
+
+def check_guess(guess)
+  guess = guess.to_i
+  number = $secret_number
+  range = 5
+  if guess > (number + range)
+    message = "Way Too High!"
+  elsif guess > number
+    message = "Too High"
+  elsif guess < (number - range)
+    message = "Way Too Low!"
+  elsif guess < number
+    message = "Too Low"
+  else
+    message = "You got it! The secret number is #{number}... I was joking about entering the matrix, ya know."
+  end
 end

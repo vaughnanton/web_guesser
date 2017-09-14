cdd9f85337639345fd6933305738c0548a6267c9
Vaughn Antonyan <vaughnanton@gmail.com>
2017-09-13 23:00:40 -0700

    I4: add guess counter functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,27 +1,47 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-$secret_number = rand(100)
+@@secret_number = rand(100)
+@@count = 5
 
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => $secret_number, :message => message}
+  count_message = check_count(guess)
+  erb :index, :locals => {:number => @@secret_number,
+                          :message => message,
+                          :count_message => count_message}
 end
 
 def check_guess(guess)
-  guess = guess.to_i
-  number = $secret_number
-  range = 5
-  if guess > (number + range)
+  if guess.nil?
+    message = ""
+  elsif  guess.to_i > (@@secret_number + 5)
     message = "You guess is way too high!"
-  elsif guess > number
+  elsif guess.to_i > @@secret_number
     message = "Your guess is too high!"
-  elsif guess < (number - range)
+  elsif guess.to_i < (@@secret_number - 5)
     message = "Your guess is way too low!"
-  elsif guess < number
+  elsif guess.to_i < @@secret_number
     message = "Your guess is too low!"
-  else
-    message = "You got it! The secret number is #{number}... I was joking about entering the matrix, ya know."
+  elsif guess.to_i == @@secret_number
+    message = "Got it! The secret number is #{@@secret_number}... I was joking about entering the matrix, ya know."
+  end
+end
+
+def check_count(guess)
+  if guess.nil?
+    ""
+  elsif @@count > 1 && guess.to_i != @@secret_number
+    @@count = @@count - 1
+    "You have #{@@count} guesses left!"
+  elsif @@count > 0 && guess.to_i == @@secret_number
+    @@count = 5
+    @@secret_number = rand(100)
+    "I've generated a new number if you would like to play again."
+  elsif @@count > 0 && guess.to_i != @@secret_number
+    @@count = 5
+    @@secret_number = rand(100)
+    "You are out of guesses. Let me think of a new number... Okay, try again!"
   end
 end
